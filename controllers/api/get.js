dc294eb162a5cb38cc74d2b1692aeb7e99694d71
fan19900404 <fan19900404@163.com>
2018-03-03 15:47:48 +0800

    申请信道方法完成

--- controllers/api/get.js
+++ controllers/api/get.js
@@ -1,8 +1,53 @@
-const Router = require('koa-router');
+const Router = require("koa-router");
+const md5 = require("../../utils/md5");
+const { check } = require("../../utils/signature");
+const config = require("../../config/index");
 const router = new Router();
 
-router.post('/get/wsurl',(ctx)=>{
-  console.log(ctx);
-})
+// 计算出信道Id
+const computeTunnelId = token => {
+  const now = +new Date();
+  let id = md5(`${now}${token}`);
+  if (!global.tunnels[id]) {
+    // 防止重复
+    return id;
+  }
+  return computeTunnelId(id);
+};
+
+// 业务服务器需要提供，token，url，signature
+router.post("/get/wsurl", ctx => {
+  console.log(global.tunnels);
+  const { token, url, signature } = ctx.request.body;
+  if (!token || !url || !signature) {
+    ctx.body = {
+      code: -1,
+      msg: "参数不正确"
+    };
+    return false;
+  }
+
+  if (!check(`${token}${url}`, signature)) {
+    ctx.body = { code: -1, msg: "签名错误" };
+    return false;
+  }
+
+  const tunnelId = computeTunnelId(token);
+
+  global.tunnels[tunnelId] = {
+    tunnelId,
+    url,
+    token,
+    createTime: +new Date()
+  };
+
+  ctx.body = {
+    code: 0,
+    data: {
+      tunnelId,
+      url: config.host +':'+ config.port + "/weapp"
+    }
+  };
+});
 
 module.exports = router;
