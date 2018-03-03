ec9541740854593e24e849269eaeff10b5b0ef95
fan19900404 <fan19900404@163.com>
2018-03-03 15:46:35 +0800

    配置文件添加一个域名参数

--- config/index.js
+++ config/index.js
@@ -11,6 +11,7 @@ const config = {
   name: pkg.name, // 应用名称
   version: pkg.version, // 版本
   bindingHost: '0.0.0.0', // 绑定IPv4
+  host:'127.0.0.1',// tunnel服务域名
   port: process.env.PORT || 3000, // 端口
   tunnelSignatureKey: 'x89dk@138xk$x%19',// 信道签名
 };
