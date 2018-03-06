1447c090bab73dc44ba6aaf403720285f88c430c
zhufan <125920969@qq.com>
2018-03-06 17:54:02 +0800

    增加删除链接功能，防止内存溢出

--- ws/index.js
+++ ws/index.js
@@ -8,9 +8,13 @@ const ws = io => {
     const tunnel = global.tunnels[tunnelId];
     tunnel.socket = socket;
     socket.on("msg", function(data) {
-      console.log(data);
       axios.post(tunnel.url, { tunnelId, token, data }).then(res => res);
     });
+
+    socket.on('disconnect',function(){
+      // 删除不存在的链接，防止内存溢出
+      delete global.tunnels[tunnelId];
+    })
   });
 
   // 凡是不符合的全部关掉
